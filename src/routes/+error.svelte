f174e240fe3d40b8818b6e5c9c073157ae7e9684
Oliver Vier <119545999+OliverVier@users.noreply.github.com>
2024-09-18 23:35:18 +0200

    add error page, rem index.ts

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+{#if $page.status = 404}
+    <h1>This is not the page you were looking for</h1>
+{/if}
+
+{#if $page.status >= 500}
+    <h1>Unexpected server error</h1>
+{/if}
